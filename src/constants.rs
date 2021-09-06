32169ed200a7f4289e18b4ee23db1fb4353bb40b
walker <warriorsfly@gmail.com>
2021-09-06 14:31:37 +0800

    add casbin dependence

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,6 @@
 /// js toISOString() in test suit can't handle chrono's default precision
 pub const DATE_FORMAT: &str = "%Y-%m-%dT%H:%M:%S%.3fZ";
+pub const BEARER_PREFIX: &str = "Bearer ";
+
+pub const MODEL_PATH: &str = "model.conf";
+pub const POLICY_PATH: &str = "policy.csv";
